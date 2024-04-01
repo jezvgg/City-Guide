38cc36ce34be3c037420d23c55a4e740df8da9ce
jezv <jezv@yandex.ru>
2024-04-01 18:10:24 +0800

    add find image by prompt

--- CLIP/CLIP.py
+++ CLIP/CLIP.py
@@ -4,6 +4,7 @@ from PIL import Image
 from io import BytesIO
 import base64
 import time
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class CLIP:
@@ -22,17 +23,42 @@ class CLIP:
         return img
 
 
-    def classificate_images(self, images: list, classes: list[str]) -> str:
+    def __decode_images(self, images: list):
         start = time.time()
-        images = [self.__decode_image(bs4url[2:-1]) for bs4url in images]
-        print(images[0])
+        result = [self.__decode_image(bs4url[2:-1]) for bs4url in images]
         print("images decoded for", time.time()-start)
+        return result
+
+
+    def classificate_images(self, images: list, classes: list[str]) -> str:
+        images = self.__decode_images(images)
+
         start = time.time()
         with torch.no_grad():
             text_latents = self.__predictor.get_text_latents(classes)
             pred_labels = self.__predictor.run(images, text_latents)
         print("predicted for", time.time() - start)
         return pred_labels
+
+
+    def get_by_prompt(self, images: list, prompt: str):
+        images = self.__decode_images(images)
+
+        start = time.time()
+        # Заменить список на массив из numpy
+        with torch.no_grad():
+            prompt_latent = self.__predictor.get_text_latents([prompt]).cpu().detach().numpy()[0].reshape(1, -1)
+            image_latents = self.__predictor.get_image_latents(images).cpu().detach().numpy()
+            result = []
+            for latent in image_latents: 
+                # Возможно эффективнее будет сразу в косинусуво сходство засунуть 2 списка и взять диагональ
+                image_latent = latent.reshape(1, -1)
+                result.append(cosine_similarity(prompt_latent, image_latent)[0][0])
+        image_index = result.index(max(result))
+        print("Find for", time.time()-start)
+        return images[image_index], image_index
+
+
 
 
     @property
--- Tests/test_clip.py
+++ Tests/test_clip.py
@@ -1,17 +1,35 @@
 import unittest
+import numpy as np
 import pandas as pd
 from CLIP.CLIP import CLIP
 from sklearn.metrics import accuracy_score
+import os
+
+os.environ["TQDM_DISABLE"] = "1"
 
 
 class test_clip(unittest.TestCase):
 
     model = CLIP()
+    data = pd.read_csv('Tests/photos_v3.csv.xz')
 
     def test_classificator(self):
-        data = pd.read_csv('Tests/photos_v3.csv.xz')
+        # 20 секунд
+        data = self.data.copy()
         classes = list(data['category'].unique())
         data['category'] = data['category'].apply(lambda x: classes.index(x))
         pred = self.model.classificate_images(data['img'], classes)
         data['pred'] = pred
         print("\n\nResult:", accuracy_score(data['category'], data['pred']))
+
+    def test_getting_by_prompt(self):
+        # 15 секунд на каждый промпт
+        data = self.data.copy()
+        prompts = list(data['name'].unique())
+        pt = 0
+        for prompt in prompts:
+            answer = np.where(data['name'] == prompt)[0]
+            img, index = self.model.get_by_prompt(data['img'], prompt)
+            if index in answer: pt+=1
+        print("\n\nResult:", len(prompts)/100 * pt, '%')
+            
