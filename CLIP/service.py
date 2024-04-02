0eb00b34c5c0d6a3c83432baf67f1d492068b175
jezv <jezv@yandex.ru>
2024-04-02 21:13:22 +0800

    fix2

--- CLIP/service.py
+++ CLIP/service.py
@@ -13,7 +13,7 @@ data = pd.read_csv('Tests/photos_v3.csv.xz')
 def service():
     args = request.args
     if 'image' in args:
-        description, index = model.get_by_prompt(list(data['img'].unique()), data['img'])
+        description, index = model.get_by_image(data['name'], args['image'])
         return app.response_class(
             response=str(index),
             status=200,
