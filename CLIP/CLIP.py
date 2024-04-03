b633d346ade5483ca3a2cf8c18ad64e15eab66c0
jezv <jezv@yandex.ru>
2024-04-03 18:08:01 +0800

    done #1

--- CLIP/CLIP.py
+++ CLIP/CLIP.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import numpy as np
 import base64
 import time
+import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 
@@ -12,11 +13,19 @@ class CLIP:
     __model = None
     __processor = None
     __predictor = None
+    text_latents = []
+    images_latents = []
 
     def __init__(self, templates = ['{}', 'это {}', 'на фото {}'], model_name = "ruclip-vit-base-patch32-384"):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.__model, self.__processor = ruclip.load(model_name, device=self.device)
         self.__predictor = ruclip.Predictor(self.__model, self.__processor, device=self.device, bs=8, templates=templates)
+
+        data = pd.read_csv('Tests/photos_v3.csv.xz')
+        images = self.__decode_images(data['img'])
+        with torch.no_grad():
+            self.images_latents = self.__predictor.get_image_latents(images).cpu().detach().numpy()
+            self.text_latents = self.__predictor.get_text_latents(data['name']).cpu().detach().numpy()
 
 
     def __decode_image(self, bs4):
@@ -31,45 +40,30 @@ class CLIP:
         return result
 
 
-    def classificate_images(self, images: list, classes: list[str]) -> str:
-        images = self.__decode_images(images)
-
-        start = time.time()
-        with torch.no_grad():
-            text_latents = self.__predictor.get_text_latents(classes)
-            pred_labels = self.__predictor.run(images, text_latents)
-        print("predicted for", time.time() - start)
-        return pred_labels
-
-
-    def get_by_prompt(self, images: list, prompt: str):
-        images = self.__decode_images(images)
+    def get_by_prompt(self, prompt: str):
 
         start = time.time()
         with torch.no_grad():
             prompt_latent = self.__predictor.get_text_latents([prompt]).cpu().detach().numpy()
-            image_latents = self.__predictor.get_image_latents(images).cpu().detach().numpy()
         print("Vectorized for:", time.time() - start)
         start = time.time()
-        result = cosine_similarity(prompt_latent, image_latents)[0]
+        result = cosine_similarity(prompt_latent, self.images_latents)[0]
         print("Cosinus for", time.time() - start)
         index = np.argmax(result)
-        return images[index], index
+        return index
 
 
-    def get_by_image(self, descriptions: list, user_image: str):
-        user_image = self.__decode_image(user_image)
+    def get_by_image(self, user_image: str):
 
         start = time.time()
         with torch.no_grad():
             user_image_latent = self.__predictor.get_image_latents([user_image]).cpu().detach().numpy()
-            latent_descriptions = self.__predictor.get_text_latents(descriptions).cpu().detach().numpy()
         print("Vectorized for:", time.time() - start)
         start = time.time()
-        similarity_scores = cosine_similarity(user_image_latent, latent_descriptions)[0]
+        similarity_scores = cosine_similarity(user_image_latent, self.text_latents)[0]
         print("Cosins in", time.time() - start, "seconds")
         index = np.argmax(similarity_scores)
-        return descriptions[index], index
+        return index
 
 
     @property
--- CLIP/Tests/test_clip.py
+++ CLIP/Tests/test_clip.py
@@ -10,14 +10,6 @@ class test_clip(unittest.TestCase):
     model = CLIP()
     data = pd.read_csv('Tests/photos_v3.csv.xz')
 
-    def test_classificator(self):
-        # 20 секунд
-        data = self.data.copy()
-        classes = list(data['category'].unique())
-        data['category'] = data['category'].apply(lambda x: classes.index(x))
-        pred = self.model.classificate_images(data['img'], classes)
-        data['pred'] = pred
-        print("\n\nResult:", accuracy_score(data['category'], data['pred']))
 
     def test_getting_by_prompt(self):
         # 15 секунд на каждый промпт
@@ -26,9 +18,10 @@ class test_clip(unittest.TestCase):
         pt = 0
         for prompt in prompts:
             answer = np.where(data['name'] == prompt)[0]
-            img, index = self.model.get_by_prompt(data['img'], prompt)
+            index = self.model.get_by_prompt(prompt)
             if index in answer: pt+=1
         print("\n\nResult:", len(prompts)/100 * pt, '%')
+
 
     def test_getting_by_image(self):
         data = self.data.copy()
@@ -38,7 +31,7 @@ class test_clip(unittest.TestCase):
         for image in images:
             description = data[data['img'] == image]['name'].iloc[0]
             correct_index = np.where(data['name'] == description)[0]
-            des, index = self.model.get_by_image(data['name'], image)
+            index = self.model.get_by_image(image)
 
             if index in correct_index:
                 correct_predictions += 1
