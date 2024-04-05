2fb44c8b0124533fba3c10180e8e5c2933a23382
Vsevolod Levitan <levvsev@gmail.com>
2024-04-06 01:35:28 +0800

    Added PlacesAPI project

--- uTraverse.AspireHost/Program.cs
+++ uTraverse.AspireHost/Program.cs
@@ -1,5 +1,8 @@
 // Create the application builder
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add the API project dependency
+builder.AddProject<Projects.uTraverse_PlacesAPI>("utraverse-placesapi");
+
 // Run the app (Aspire AppHost)
 builder.Build().Run();
