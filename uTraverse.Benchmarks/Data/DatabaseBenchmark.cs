552335bed216778887e8fba695137ad4b7a231e0
Vsevolod Levitan <levvsev@gmail.com>
2024-04-06 16:19:49 +0800

    Added benchmark project

---
+++ uTraverse.Benchmarks/Data/DatabaseBenchmark.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+using uTraverse.PlacesAPI.Models;
+
+namespace uTraverse.Benchmarks.Data;
+
+internal class DatabaseBenchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+
+    }
+}
