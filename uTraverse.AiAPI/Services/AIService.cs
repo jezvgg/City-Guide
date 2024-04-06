b2b6027622bfcf130ef5ee5b59033fe2dee4b8af
Vsevolod Levitan <levvsev@gmail.com>
2024-04-06 09:39:58 +0800

    Extracted interface for the AI service

--- uTraverse.AiAPI/Services/AIService.cs
+++ uTraverse.AiAPI/Services/AIService.cs
@@ -7,17 +7,12 @@ namespace uTraverse.AiAPI.Services;
 /// </summary>
 /// <param name="logger">A logger instance for internal usage</param>
 /// <param name="httpClient">An HttpClient instance for communication with the AI microservice (should have BaseAddress set to the AI microservice URL)</param>
-public class AIService (ILogger<AIService> logger, HttpClient httpClient)
+public class AIService (ILogger<AIService> logger, HttpClient httpClient) : IAIService
 {
     private readonly ILogger _logger = logger;
     private readonly HttpClient _httpClient = httpClient;
 
-    /// <summary>
-    /// Retrieves an array of place IDs matching the given prompt
-    /// </summary>
-    /// <param name="prompt">The prompt to match the places</param>
-    /// <returns>An array of place IDs that match the prompt</returns>
-    public async Task<Guid[]> GetPlaceIdsAsync(string prompt)
+    public async Task<Guid[]> GetPlaceIdsAsync (string prompt)
     {
         // TODO: Add distributed caching to offload the AI and speed up execution
 
---
+++ uTraverse.AiAPI/Services/IAIService.cs
@@ -0,0 +1,14 @@
+﻿namespace uTraverse.AiAPI.Services;
+
+/// <summary>
+/// Manages communication with the AI microservice
+/// </summary>
+public interface IAIService
+{
+    /// <summary>
+    /// Retrieves an array of place IDs matching the given prompt
+    /// </summary>
+    /// <param name="prompt">The prompt to match the places</param>
+    /// <returns>An array of place IDs that match the prompt</returns>
+    Task<Guid[]> GetPlaceIdsAsync (string prompt);
+}
