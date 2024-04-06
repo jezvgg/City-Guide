247f450a1722a242e97603d5629d4384b392883d
Vsevolod Levitan <levvsev@gmail.com>
2024-04-06 08:18:47 +0800

    Added place matcher API container to the Aspire host

--- uTraverse.AspireHost/Program.cs
+++ uTraverse.AspireHost/Program.cs
@@ -1,11 +1,14 @@
 // Create the application builder
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add the place matcher AI container
+var placeMatcher = builder.AddContainer("utraverse-placematcher", "utraverse/placematcher");
+
 // Add the Places API project reference
-builder.AddProject<Projects.uTraverse_PlacesAPI>("utraverse-placesapi");
+var placesApi = builder.AddProject<Projects.uTraverse_PlacesAPI>("utraverse-placesapi");
 
 // Add the AI API project reference
-builder.AddProject<Projects.uTraverse_AiAPI>("utraverse-aiapi");
+var aiApi = builder.AddProject<Projects.uTraverse_AiAPI>("utraverse-aiapi");
 
 // Run the app (Aspire AppHost)
 builder.Build().Run();
