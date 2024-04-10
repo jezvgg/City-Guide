3e5ef4b94c0289bd26a57a328e6830c669ed6e84
Aizek <129169524+AItEKS@users.noreply.github.com>
2024-04-11 04:54:05 +0800

    Update test_clip.py

--- CLIP/Tests/test_clip.py
+++ CLIP/Tests/test_clip.py
@@ -28,6 +28,26 @@ class test_clip(unittest.TestCase):
                     'prompts':'Tests/prompts_yrl.csv',
                     'data':'Yaroslavl_images_clean.csv'}]
 
+    images_test = [{'benchmark_name':'Иркутск',
+                    'index':'irkutsk.index',
+                    'data':'photos_v3.csv.xz',
+                    'test_data': 'irk_validate.csv'},
+                    {'benchmark_name':'Нижний Новгород',
+                    'index':'niznii_novgorod.index',
+                    'data':'NN_images_clean_test.csv',
+                    'test_data': 'NN_validate.csv'},
+                    {'benchmark_name':'Владимир',
+                    'index':'vladimir.index',
+                    'data':'Vladimir_images_clean.csv',
+                    'test_data': 'Vladimir_validate.csv'},
+                    {'benchmark_name':'Екатеренбург',
+                    'index':'ekaterinburg.index',
+                    'data':'EKB_images_clean_test.csv',
+                    'test_data': 'EKB_validate.csv'},
+                    {'benchmark_name':'Ярославль',
+                    'index':'yaroslavl.index',
+                    'data':'Yaroslavl_images_clean_test.csv',
+                    'test_data': 'Yaroslavl_validate.csv'}]
 
     def test_getting_by_prompt(self):
         model = CLIP('irkutsk.index')
@@ -43,7 +63,7 @@ class test_clip(unittest.TestCase):
                 indexes = model.get_by_prompt(prompt)
 
                 print(data.iloc[indexes[0]]['name'], '|', name, '|', prompt)
-                if data.iloc[indexes[0]]['name'] == name: 
+                if data.iloc[indexes[0]]['name'] == name:
                     correct+=1
                 times.append(time()-start)
 
@@ -56,21 +76,23 @@ class test_clip(unittest.TestCase):
     def test_getting_by_image(self):
         model = CLIP('Tests/val.index')
 
-        # 70%
+        for benhmark in self.image_test:
+            model.set_index(benhmark['index'])
+            data = pd.read_csv(benhmark['data'])
+            test_data = pd.read_csv(benhmark['test_data'])
 
-        data = pd.read_csv('Tests/val_img_data.csv')
-        images = pd.read_csv('Tests/test_img_data.csv')
+            correct = 0
+            times = []
+            for image, name in zip(test_data['img'], test_data['name']):
+                start = time()
+                indexes = model.get_by_image(image)
 
-        correct_predictions = 0
+                print(data.iloc[indexes[0]]['name'], '|', name, '|', image)
+                if data.iloc[indexes[0]]['name'] == name:
+                    correct+=1
+                times.append(time()-start)
 
-        for image, name in zip(images['img'], images['name']):
-
-            index = model.get_by_image(image)
-
-            if data.iloc[index]['name'] == name:
-                correct_predictions += 1
-
-        accuracy = (correct_predictions / len(images)) * 100
-        print("\n\nAccuracy:", accuracy, '%')
-
-            
+            print('\n',benhmark['benchmark_name'])
+            print('Mean time:', sum(times) / len(times))
+            accuracy = (correct / len(test_data)) * 100
+            print("Result:", accuracy, '%')
