40c7693ac7cb17b4c5b32ed2730a8ce5efe1610a
Vsevolod Levitan <levvsev@gmail.com>
2024-04-10 12:55:55 +0300

    Added waves, refactored start animation scripts

--- Frontend/scripts.js
+++ Frontend/scripts.js
@@ -1,53 +1,59 @@
-var timeout = 1000;
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+document.addEventListener("DOMContentLoaded", async function () {
+    var orlogobar = $("div.logobar:not(#alternative)");
+    if (Math.random() < 0.05) {
+        var logobar = $(".logobar");
+        var altlogobar = $("#alternative.logobar");
+        var altnavbar = $("#alternative.navbar");
 
-if (Math.random() <= 0.05) {
-    document.addEventListener("DOMContentLoaded", function () {
-        $(".logobar").hide().css("opacity", "0%");
-        $("#alternative.logobar").show().css("opacity", "100%");
-        timeout = 2200;
+        // Hide usual logo bar
+        logobar.hide().css("opacity", "0%");
+        // Show alternative logo bar
+        altlogobar.show().css("opacity", "100%");
+
+        await sleep(500);
+
+        // Slowly hide alternative logobar text
+        altnavbar.css("opacity", "0%");
+
+        await sleep(500);
+
+        // Display: none on alternative logobar text
+        altnavbar.hide();
+        // Move alternative logobar to the left
+        altlogobar.css("left", "150%");
+
+        await sleep(500);
+
+        // Hide alternative logobar completely, move it to the top-left
+        altlogobar.hide().css("left", "-5%").css("top", "5%").css("transition", "all 3s");
+        // Also move usual logobar to the top-left and show it
+        orlogobar.css("left", "-10%").css("top", "3%").show();
+
+        await sleep(1);
+
+        // Show alternative logobar and move it off the screen to the left
+        altlogobar.show().css("left", "150%");
+        // Hide the text on alternative logobar
+        altnavbar.hide();
+    }
+
+    await sleep(199);
+
+    // Fade in the original logobar
+    orlogobar.show().css("opacity", "100%");
+
+    // Reset the position on the original logobar
+    orlogobar.css("left", "").css("top", "").show();
+
+    // Show start animations
+    var items = document.querySelectorAll(".startanim");
+    items.forEach(elem => {
+        elem.classList.add("end");
     });
 
-    document.addEventListener("DOMContentLoaded", function () {
-        setTimeout(function () {
-            $(".navbar#alternative").css("opacity", "0%");
-        }, 500)
-    });
+    await sleep(1000);
 
-    document.addEventListener("DOMContentLoaded", function () {
-        setTimeout(function () {
-            $(".navbar#alternative").hide();
-            $("#alternative.logobar").css("left", "150%");
-            console.log("aaa");
-        }, 1000)
-    });
-
-    document.addEventListener("DOMContentLoaded", function () {
-        setTimeout(function () {
-            $("div#alternative.logobar").hide().css("left", "-5%").css("top", "5%").css("transition", "all 3s");
-            $("div.logobar:not(#alternative)").css("left", "-10%").css("top", "3%").show();
-            console.log("aaa");
-        }, 2000)
-    });
-
-    document.addEventListener("DOMContentLoaded", function () {
-        setTimeout(function () {
-            $("#alternative.logobar").show().css("left", "150%");
-            $(".navbar#alternative").hide();
-        }, 2001)
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        $(".logobar:not(#alternative)").show().css("opacity", "100%");
-
-        $("div.logobar:not(#alternative)").css("left", "").css("top", "").show();
-
-        $(".blur-cover").hide();
-
-        var items = document.querySelectorAll(".startanim");
-        items.forEach(elem => {
-            elem.classList.add("end");
-        });
-    }, timeout);
+    // Hide the fullscreen blur cover
+    $(".blur-cover").hide();
 });
