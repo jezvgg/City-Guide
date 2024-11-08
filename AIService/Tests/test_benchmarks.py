af2e65cf370b12404f94c7889319591a2a7b8b7e
jezv <jezv@yandex.ru>
2024-11-08 21:36:31 +0800

    Fix bugs, fixing data, add configuration to benchmarks, fix image tests

--- AIService/Tests/test_benchmarks.py
+++ AIService/Tests/test_benchmarks.py
@@ -1,9 +1,12 @@
 import unittest
-import pandas as pd
 from time import time
 from pathlib import Path
-from Model import CLIP
 import logging
+import json
+
+import pandas as pd
+
+from Model import CLIP
 
 
 
@@ -12,46 +15,14 @@ class test_benchmarks(unittest.TestCase):
     cwf = Path(__file__).resolve().parent
     cwbd = cwf / 'Benchmarks' # Current Working Benhmarks Directory
 
-    # Download data for benchmarks and unzip to ./Tests/Benchmarks
+    # Old data
     # https://drive.google.com/file/d/1ae7ehbyXEeCwN5gkfSH6zIAW6V5sCKdV/view?usp=sharing
+    # New data
+    # https://drive.google.com/file/d/1wSJKfq1msri_w0op2VE9shsaNhxPdW2U/view?usp=sharing
 
-    prompt_test = [{'benchmark_name':'Иркутск',
-                    'index':'irkutsk.index',
-                    'prompts': cwbd / 'prompts.csv.xz',
-                    'data': cwbd / 'photos_v3.csv.xz'},
-                    {'benchmark_name':'Нижний Новгород',
-                    'index':'niznii_novgorod.index',
-                    'prompts':cwbd / 'prompts_NN.csv.xz',
-                    'data':cwbd / 'NN_images_clean.csv.xz'},
-                    {'benchmark_name':'Владимир',
-                    'index':'vladimir.index',
-                    'prompts':cwbd / 'prompt_Vlad.csv.xz',
-                    'data':cwbd / 'Vladimir_images_clean.csv.xz'},
-                    {'benchmark_name':'Екатеренбург',
-                    'index':'ekaterinburg.index',
-                    'prompts':cwbd / 'prompts_EKB.csv.xz',
-                    'data':cwbd / 'EKB_images_clear.csv.xz'},
-                    {'benchmark_name':'Ярославль',
-                    'index':'yaroslavl.index',
-                    'prompts':cwbd / 'prompts_yrl.csv.xz',
-                    'data':cwbd / 'Yaroslavl_images_clean.csv.xz'}]
 
-    images_test = [{'benchmark_name':'Нижний Новгород',
-                    'index':'niznii_novgorod.index',
-                    'data':cwbd / 'NN_images_clean_test.csv.xz',
-                    'test_data': cwbd / 'NN_validate.csv.xz'},
-                    {'benchmark_name':'Владимир',
-                    'index':'vladimir.index',
-                    'data':cwbd / 'Vladimir_images_clean.csv.xz',
-                    'test_data': cwbd / 'Vladimir_validate.csv.xz'},
-                    {'benchmark_name':'Екатеренбург',
-                    'index':'ekaterinburg.index',
-                    'data':cwbd / 'EKB_images_clean_test.csv.xz',
-                    'test_data': cwbd / 'EKB_validate.csv.xz'},
-                    {'benchmark_name':'Ярославль',
-                    'index':'yaroslavl.index',
-                    'data':cwbd / 'Yaroslavl_images_clean.csv.xz', # некорректный тест
-                    'test_data': cwbd / 'Yaroslavl_validate.csv.xz'}]
+    with open(cwbd / 'text' / 'config.json') as f: prompt_test = json.load(f)
+    with open(cwbd / 'image' / 'config.json') as f: images_test = json.load(f)
 
 
     def test_getting_by_prompt(self):
@@ -60,18 +31,18 @@ class test_benchmarks(unittest.TestCase):
         mean_accuracy = []
         for benhmark in self.prompt_test:
             model.set_index(benhmark['index'])
-            prompts = pd.read_csv(benhmark['prompts'])
-            data = pd.read_csv(benhmark['data'])
+            prompts = pd.read_csv(self.cwbd / 'text' / benhmark['prompts'])
+            data = pd.read_csv(self.cwbd / 'text' / benhmark['data'])
 
             correct = 0
             times = []
             for prompt, name in zip(prompts['prompts'], prompts['name']):
                 start = time()
                 indexes = model.get_by_prompt(prompt)
-
-                if data.iloc[indexes[0]]['name'] == name:
-                    correct+=1
                 times.append(time()-start)
+
+                if data.loc[indexes[0]]['name'] == name:
+                    correct+=1
             
             mean_time = sum(times) / len(times)
             times_bench.append(mean_time)
@@ -90,23 +61,23 @@ class test_benchmarks(unittest.TestCase):
         for benhmark in self.images_test:
             print('\n',benhmark['benchmark_name'])
             model.set_index(benhmark['index'])
-            data = pd.read_csv(benhmark['data'])
-            test_data = pd.read_csv(benhmark['test_data'])
+            data = pd.read_csv(self.cwbd / 'image' / benhmark['data'], index_col=0)
+            test_data = pd.read_csv(self.cwbd / 'image' / benhmark['test_data'], index_col=0)
 
             correct = 0
             times = []
-            for image, name in zip(test_data['img'], test_data['name']):
+            for i, image, name in zip(test_data.index, test_data['img'], test_data['name']):
                 start = time()
                 image = CLIP.decode_image(image)
                 indexes = model.get_by_image(image)
 
-                if indexes[0] >= data.shape[0]:
-                    print("Error at", indexes[0], data.shape[0])
-                    continue
-
-                if data.iloc[indexes[0]]['name'] == name:
-                    correct+=1
                 times.append(time()-start)
+
+                index = indexes[0] if indexes[0] != i else indexes[1]
+                if index not in data.index: continue # Заменить когда буду прикручивать новую БД
+
+                if data.loc[index]['name'] == name:
+                    correct+=1
 
             mean_time = sum(times) / len(times)
             times_bench.append(mean_time)
